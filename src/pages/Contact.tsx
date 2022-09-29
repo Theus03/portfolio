e7ca1b691d38b7a086d010e68167cc71a98d87a5
Matheus Leite <matheus.leite@3stecnologia.com.br>
2022-09-29 18:30:32 -0300

    Create page Contact in my portfolio

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,26 @@
+import outlook from "../assets/outlook.png"
+import gmail from "../assets/gmail.png"
+import linkedin from "../assets/linkedin.png"
+
+
 export function Contact() {
     return(
-        <h1>Contact</h1>
+        <div className="container-contact">
+            <main>
+                <h2 className="title-contact">Contact Me</h2>
+                <div className="box-contact">
+                    <img className="outlook" src={outlook}></img>
+                    <a>matheusssleite@outlook.com</a>
+                </div>
+                <div className="box-contact">
+                    <img className="gmail" src={gmail}></img>
+                    <a>matheusflute03@gmail.com</a>
+                </div>
+                <div className="box-contact">
+                    <img className="linkedin" src={linkedin}></img>
+                    <a>@matheusssleite</a>
+                </div>
+            </main>
+        </div>
     )
 }
